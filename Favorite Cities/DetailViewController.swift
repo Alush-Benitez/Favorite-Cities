ab2b97aea9fc37154bcd9e590fc2509c34d21291
Alush Benitez <alushb5@yahoo.com>
2018-07-09 10:10:16 -0500

    Connected UI Object to storyboard

--- Favorite Cities/DetailViewController.swift
+++ Favorite Cities/DetailViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var populationTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var detailItem: City? {
+        didSet {
+            // Update the view.
+            configureView()
         }
     }
 
@@ -27,16 +27,29 @@ class DetailViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         configureView()
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        if let city = self.detailItem {
+            city.name = cityTextField.text!
+            city.state = stateTextField.text!
+            city.population = Int(populationTextField.text!)!
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    var detailItem: NSDate? {
-        didSet {
-            // Update the view.
-            configureView()
+    
+    func configureView(){
+        // Update the user interface for the detail item
+        if let city = self.detailItem {
+            if cityTextField != nil {
+                cityTextField.text = city.name
+                stateTextField.text = city.state
+                populationTextField.text = String(city.population)
+                imageView.image = UIImage(data: city.image)
+            }
         }
     }
 
